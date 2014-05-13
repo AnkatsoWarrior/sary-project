479dea67bd4b902c1647b0ecd02d8e74bb138f80
Nabil <nabil.projets@gmail.com>
2014-05-13 19:47:14 +0100

    Version checker added !

--- src/main/java/org/sary/Sary.java
+++ src/main/java/org/sary/Sary.java
@@ -1,19 +1,19 @@
 /*
-* Copyright (C) 2014 Nabil Andriantomanga <nabil.arrowbase at gmail.com>
-*
-* This library is free software; you can redistribute it and/or
-* modify it under the terms of the GNU Lesser General Public
-* License as published by the Free Software Foundation; either
-* version 2.1 of the License, or (at your option) any later version.
-*
-* This library is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with this library. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (C) 2014 Nabil Andriantomanga <nabil.arrowbase at gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.sary;
 
 import java.awt.Color;
@@ -28,90 +28,126 @@ import org.sary.form.SaryForm;
 import org.sary.form.SaryFormat;
 
 /**
-*
-* @author Nabil Andriantomanga <nabil.arrowbase at gmail.com>
-*/
+ * 
+ * @author Nabil Andriantomanga <nabil.arrowbase at gmail.com>
+ */
 public class Sary {
 
 	private SaryCanvas canvas = new SaryCanvas();
 
+	
+	/**
+	 * Objet permettant d'exporter le canvas (l'ensemble des figures avec le
+	 * fond défini par le sous forme de fichier image.
+	 * 
+	 * Ce fichier peut etre dans l'un des format suivant : jpg, png ou gif. Pour
+	 * ce faire, il suffit d'utiliser l'implémentation <tt>SaryExporter</tt> de
+	 * l'interface <tt>Exporter</tt>.
+	 */
 	private Exporter exporter = new SaryExporter();
 
+	
 	public SaryCanvas getCanvas() {
 		return canvas;
 	}
 
+	
 	public void setCanvas(SaryCanvas canvas) {
 		this.canvas = canvas;
 	}
 
+	
 	public Exporter getExporter() {
 		return exporter;
 	}
 
+	
 	public void setExporter(Exporter exporter) {
 		this.exporter = exporter;
 	}
 
+	
 	public void add(SaryForm saryForm) {
 		canvas.add(saryForm);
 	}
 
+	
 	public void remove(SaryForm saryForm) {
 		canvas.remove(saryForm);
 	}
+	
 
 	public boolean has(SaryForm saryForm) {
 		return canvas.has(saryForm);
 	}
 
+	
 	public List<SaryForm> getAll() {
 		return canvas.getAll();
 	}
 
+	
 	public boolean isEmpty(Rectangle space) {
 		return canvas.isEmpty(space);
 	}
 
+	
 	public void setBackground(Color background) {
 		canvas.setBackground(background);
 	}
 
+	
 	public void setForeground(Color foreground) {
 		canvas.setForeground(foreground);
 	}
 
+	
 	public Color getBackground() {
 		return canvas.getBackground();
 	}
 
+	
 	public Color getForeground() {
 		return canvas.getForeground();
 	}
 
+	
 	public void export(Exporter exporter) throws SaryExportException {
 		final String defaultOutput = "sary";
+		
 		if (exporter.getFormat() == null) {
+			
 			exporter.setFormat(SaryFormat.PNG);
+			
 		}
+		
 		if (exporter.getOutput() == null) {
+			
 			exporter.setOutput(defaultOutput);
+			
 		} else if (exporter.getOutput().isEmpty()) {
+			
 			exporter.setOutput(defaultOutput);
+			
 		}
+		
 		if (exporter.getCanvas() == null) {
 			exporter.setCanvas(canvas);
 		}
+		
 		exporter.export();
 	}
 
+	
 	public void export(SaryFormat format, String output)
 			throws SaryExportException {
 		this.exporter.setFormat(format).setOutput(output).setCanvas(canvas)
 				.export();
 	}
 
+	
 	public static void main(String[] args) {
-		System.out.println("Hello from Sary ! ");
+		System.out.println("Hello from Sary "
+				+ new SaryVersion().getVersionString() + " !");
 	}
 }
---
+++ src/main/java/org/sary/SaryVersion.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2014 Nabil Andriantomanga <nabil.arrowbase at gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.sary;
+
+import java.io.Serializable;
+
+
+/**
+ * Permet de savoir la version de la librairie utilisee.
+ * 
+ * @author Nabil Andriantomanga <nabil.arrowbase at gmail.com>
+ * @since 1.0
+ * 
+ */
+public class SaryVersion implements Comparable<SaryVersion>, Serializable {
+
+	private final int version = 1;
+
+	private final String versionString = "version 1.0";
+
+	private static final long serialVersionUID = 1L;
+
+	public int getVersion() {
+		return version;
+	}
+
+	public String getVersionString() {
+		return versionString;
+	}
+
+	@Override
+	public int compareTo(SaryVersion otherVersion) {
+
+		if (otherVersion.getVersion() > version) {
+			return -1;
+		}
+
+		if (otherVersion.getVersion() < version) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + version;
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SaryVersion other = (SaryVersion) obj;
+		if (version != other.version)
+			return false;
+		return true;
+	}
+
+}
